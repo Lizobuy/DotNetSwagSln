c2e199dc1e51f82f674d28bf61f2d0e2a5f70df5
Lizobuy <102720671+Lizobuy@users.noreply.github.com>
2022-05-31 13:09:07 +0200

    Ctreated tabs

--- DotNetSwag/DotNetSwag/OrderPage.xaml.cs
+++ DotNetSwag/DotNetSwag/OrderPage.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace DotNetSwag
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class OrderingPage : ContentPage
+    public partial class OrderPage : ContentPage
     {
-        public OrderingPage()
+        public OrderPage()
         {
             InitializeComponent();
         }
