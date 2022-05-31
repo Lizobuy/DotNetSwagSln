433ad3417a1d3794731be49982e3e9ba4cf62970
Lizobuya Danisa <ldanisa2@gmail.com>
2022-06-01 01:04:27 +0200

    Database added

--- DotNetSwag/DotNetSwag/DotNetSwagDatabase.cs
+++ DotNetSwag/DotNetSwag/DotNetSwagDatabase.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+
 
 namespace DotNetSwag
 {
     internal class DotNetSwagDatabase
     {
+        private SQLiteConnection _database;
+
+        public static DotNetSwagDatabase Instance = new DotNetSwagDatabase();
+
+        public DotNetSwagDatabase()
+        {
+            ///
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            path = path + "joke.db";
+
+            _database = new SQLiteConnection(path, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache);
+
+            _database.CreateTable<DotNetSwag>();
+        }
+
+        public List<DadJoke> GetJokes()
+        {
+            return _database.Table<Orders>().OrderByDescending(x => x.JokeDate).ToList();
+        }
+
+        public void SaveDotNetSwag(DadJoke joke)
+        {
+            _database.Insert(Orders);
+        }
     }
 }
---
+++ DotNetSwag/DotNetSwag/Service/Orders.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetSwag.Service
+{
+    internal class Orders
+    {
+        private readonly List<string> _orders;
+    }
+}
