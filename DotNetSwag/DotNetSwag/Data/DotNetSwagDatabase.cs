3da82b66e76fc828076e1a5425320d7067ff2711
Lizobuy <102720671+Lizobuy@users.noreply.github.com>
2022-06-01 12:36:39 +0200

    Added pages

---
+++ DotNetSwag/DotNetSwag/Data/DotNetSwagDatabase.cs
@@ -0,0 +1,57 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace DotNetSwag
+{
+    public class DotNetSwagDatabase
+    {
+        static SQLiteAsyncConnection Database;
+
+        public static readonly AsyncLazy<TodoItemDatabase> Instance = new AsyncLazy<TodoItemDatabase>(async () =>
+        {
+            var instance = new TodoItemDatabase();
+            CreateTableResult result = await Database.CreateTableAsync<TodoItem>();
+            return instance;
+        });
+
+        public DotNetSwagDatabase()
+        {
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        }
+
+        public Task<List<TodoItem>> GetItemsAsync()
+        {
+            return Database.Table<TodoItem>().ToListAsync();
+        }
+
+        public Task<List<TodoItem>> GetItemsNotDoneAsync()
+        {
+            return Database.QueryAsync<TodoItem>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
+        }
+
+        public Task<TodoItem> GetItemAsync(int id)
+        {
+            return Database.Table<TodoItem>().Where(i => i.ID == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(TodoItem item)
+        {
+            if (item.ID != 0)
+            {
+                return Database.UpdateAsync(item);
+            }
+            else
+            {
+                return Database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(TodoItem item)
+        {
+            return Database.DeleteAsync(item);
+        }
+    }
+}
